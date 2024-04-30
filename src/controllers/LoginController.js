eb7a70cfba61b8d6afcbefb58f17c8322a877da7
RebecaVerissimo <rebeca_verissimo@estudante.sesisenai.org.br>
2024-04-30 13:07:19 -0300

    adicionando criptografia alunos

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,3 +1,4 @@
+const { compare, hash } = require("bcryptjs")
 const Aluno = require('../models/Aluno')
 const { sign } = require('jsonwebtoken')
 
@@ -22,6 +23,12 @@ class LoginController {
 
             if (!aluno) {
                 return res.status(404).json({ error: 'Nenhum aluno corresponde a email e senha fornecidos!' })
+            }
+
+            const hashSenha = await compare(password, aluno.password)
+
+            if(hashSenha === false) {
+                return res.status(403).json({message: 'Aluno não encontrado'})
             }
 
             const payload = { sub: aluno.id, email: aluno.email, nome: aluno.nome }
